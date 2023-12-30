c1d025132cfb12bf02b5861f71fd58154b63de0d
ByteFlow <byteflow007@gmail.com>
2023-12-30 12:57:13 +0530

    signup route optimized

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const User = require("./modal/User/user.modal.js");
 const Course = require("./modal/Course/course.modal.js");
 const connectDB = require("./db/index.js");
 
-
 require("dotenv").config();
 connectDB();
 
@@ -16,41 +15,34 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   const admin = await Admin.find();
-
   res.json({ admin });
 });
 
 app.post("/admin/signup", async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    const admin = await Admin.findOne({
-      $or: [{ username: username }, { email: email }],
-    });
-    if (admin) {
-      return res.json({ message: "Username or Email Already Exist" });
+    const { email, username, password } = req.body;
+    const admin = await Admin.findOne({email})
+    if (!admin) {
+      await new Admin({ email, username, password }).save();
+      return res.send("Admin Registered.");
     }
-    await new Admin({ username, email, password }).save();
-    res.json({ message: "Admin created successfully" });
-  } catch (e) {
-    res.json({ error: e });
+    res.status(400).json({message: "Admin already exist!"});
+  } catch (err) {
+    res.json({ err, errMessage: "Error!" });
   }
 });
 
-app.post("/admin/login", async (req, res) => {
+app.post("/admin/signin", async (req, res) => {
   try {
-    const { usernameoremail, password } = req.body;
-    const admin = await Admin.findOne({
-      $or: [{ username: usernameoremail }, { email: usernameoremail }],
-      password: password,
-    });
-    if (admin) {
-      return res.json({ message: "Admin Login Successfull" });
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({ email, password });
+    if (!admin) {
+      return res.status(404).send("Invalid Credentials!");
     }
-    res.json({ message: "Login failed" });
-  } catch (e) {
-    res.json({ error: e });
+    res.json({ message: "Signin Successful", admin });
+  } catch (err) {
+    res.json({ err, errMessage: "Error!" });
   }
-
 });
 
 app.listen(PORT, () => {
